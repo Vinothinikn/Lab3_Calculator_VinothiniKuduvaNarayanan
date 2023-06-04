2bdd58242136c194f658e954029cc9b98b593ae6
Vinothini <knvinothini@gmail.com>
2023-06-04 15:50:42 -0400

    javascript file changes

--- offline_activity1.js
+++ offline_activity1.js
@@ -1,152 +1,120 @@
 //Global variables
-var prevCalc = 0;
-var calc = "";
-
+var preNumber = 0;
+var operator = "";
 
 //The following function displays a number in the textfield when a number is clicked.
 //Note that it keeps concatenating numbers which are clicked. 
-function showNum(value) {
-    document.frmCalc.txtNumber.value += value;
-}
+function appendNumber(number) {
+   document.getElementById("result").value += number;
+  }
 
 //The following function decreases the value of displayed number by 1.
 //isNaN method checks whether the value passed to the method is a number or not.     
 function decrement() {
-    var num = parseFloat(document.frmCalc.txtNumber.value);
-        if (!(isNaN(num))) {
-            num--;
-            document.frmCalc.txtNumber.value = num;
+    var input = parseFloat(document.getElementById("result").value);
+        if (!(isNaN(input))) {
+            input--;
+            document.getElementById("result").value = input;
         }
 }
 
 //The following function increases the value of displayed number by 1.
 function increment() {
-    var num = parseFloat(document.frmCalc.txtNumber.value);
-        if (!(isNaN(num))) {
-            num++;
-            document.frmCalc.txtNumber.value = num;
+    var input = parseFloat(document.getElementById("result").value);
+        if (!(isNaN(input))) {
+            input++;
+            document.getElementById("result").value = input;
         }
 }
 
 //The following function is called when "square root" button is clicked.
 function sqrt() {
-    var num = parseFloat(document.frmCalc.txtNumber.value);
-        if (!(isNaN(num))) {
-           var sqroot = Math.sqrt(num);
-            document.frmCalc.txtNumber.value = sqroot;
+    var input = parseFloat(document.getElementById("result").value);
+        if (!(isNaN(input))) {
+           var sqroot = Math.sqrt(input);
+           document.getElementById("result").value = sqroot;
         }
 }
 
 //The following function is called when "floor" button is clicked.
 function floor() {
-    var num = parseFloat(document.frmCalc.txtNumber.value);
-        if (!(isNaN(num))) {
-           var floor = Math.floor(num);
-            document.frmCalc.txtNumber.value = floor;
+    var input = parseFloat(document.getElementById("result").value);
+        if (!(isNaN(input))) {
+           var floor = Math.floor(input);
+           document.getElementById("result").value = floor;
         }
 }
 
 //The following function is called when "round" button is clicked.
 function round() {
-    var num = parseFloat(document.frmCalc.txtNumber.value);
-        if (!(isNaN(num))) {
-           var round = Math.round(num);
-            document.frmCalc.txtNumber.value = round;
+    var input = parseFloat(document.getElementById("result").value);
+        if (!(isNaN(input))) {
+           var round = Math.round(input);
+           document.getElementById("result").value = round;
         }
 }
 
 //The following function is called when "square" button is clicked.
 function square() {
-    var num = parseFloat(document.frmCalc.txtNumber.value);
-        if (!(isNaN(num))) {
-           var square = Math.pow(num,2);
-            document.frmCalc.txtNumber.value = square;
+    var input = parseFloat(document.getElementById("result").value);
+        if (!(isNaN(input))) {
+           var square = Math.pow(input,2);
+           document.getElementById("result").value= square;
         }
 }
 
-
-//The following function is called when "Add" button is clicked. 
-//Note that it also changes the values of the global variables.       
-function add() {
-    var num = parseFloat(document.frmCalc.txtNumber.value);
-        if (!(isNaN(num))) {
-            prevCalc = num;
-            document.frmCalc.txtNumber.value = "";
-            calc = "Add";
+function performOperation(operand){
+    var input = parseFloat(document.getElementById("result").value);
+    if (!(isNaN(input))) {
+        preNumber = input;
+        document.getElementById("result").value = "";  
+        if(operand == "+"){
+            operator = "Add";
+        } 
+        else if(operand == "-"){
+            operator = "Subtract";
+        }else if(operand == "*"){
+            operator = "mul";
         }
-}
-
-//The following function is called when "Subtract" button is clicked. 
-function subtract() {
-    var num = parseFloat(document.frmCalc.txtNumber.value);
-        if (!(isNaN(num))) {
-            prevCalc = num;
-            document.frmCalc.txtNumber.value = "";
-            calc = "Subtract";
+        else if(operand == "/"){
+            operator = "div";
         }
-}
-
-//The following function is called when "Multiply" button is clicked. 
-function mul() {
-    var num = parseFloat(document.frmCalc.txtNumber.value);
-        if (!(isNaN(num))) {
-            prevCalc = num;
-            document.frmCalc.txtNumber.value = "";
-            calc = "mul";
+        else if(operand == "^"){
+            operator = "power";
         }
+    }
 }
-
-//The following function is called when "division" button is clicked. 
-function div() {
-    var num = parseFloat(document.frmCalc.txtNumber.value);
-        if (!(isNaN(num))) {
-            prevCalc = num;
-            document.frmCalc.txtNumber.value = "";
-            calc = "div";
-        }
-}
-
-//The following function is called when "power (^)" button is clicked. 
-function power() {
-    var num = parseFloat(document.frmCalc.txtNumber.value);
-        if (!(isNaN(num))) {
-            prevCalc = num;
-            document.frmCalc.txtNumber.value = "";
-            calc = "power";
-        }
-}
-
 //The following function is called when "Calculate" button is clicked.
 //Note that this function is dependent on the value of global variable.        
 function calculate() {
-    var num = parseFloat(document.frmCalc.txtNumber.value);
+    var input = parseFloat(document.getElementById("result").value);
     var total = 0;
-        if (!(isNaN(num))) {
-            if (calc == "Add"){
-               total  = prevCalc + num;
-                document.frmCalc.txtNumber.value = total;
+        if (!(isNaN(input))) {
+            if (operator == "Add"){
+               total  = preNumber + input;
+               document.getElementById("result").value = total;
             }
-            else if(calc == "Subtract"){
-                 total = prevCalc - num;
-                document.frmCalc.txtNumber.value = total;
+            else if(operator == "Subtract"){
+                 total = preNumber - input;
+                 document.getElementById("result").value = total;
             }
-            else if(calc == "mul"){
-                 total = prevCalc * num;
-                document.frmCalc.txtNumber.value = total;
+            else if(operator == "mul"){
+                 total = preNumber * input;
+                 document.getElementById("result").value = total;
             }
-            else if(calc == "div"){
-                 total = prevCalc / num;
-                document.frmCalc.txtNumber.value = total;
+            else if(operator == "div"){
+                 total = preNumber / input;
+                 document.getElementById("result").value = total;
             }
-            else if(calc == "power"){
-                var power = Math.pow(prevCalc,num);
-                document.frmCalc.txtNumber.value = power;
+            else if(operator == "power"){
+                 total = Math.pow(preNumber,input);
+                document.getElementById("result").value = total;
             }
             
 }}
 
 function clear() {
-	document.frmCalc.txtNumber.value = "";
-	prevCalc = 0;
-	calc = "";
+	document.getElementById("result").value  = "";
+	preNumber = 0;
+	operator = "";
 }
